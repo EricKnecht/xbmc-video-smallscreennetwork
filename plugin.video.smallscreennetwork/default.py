c47e77970415a58bbad2c9d8877b19d8ca9c6e9f
Eric Knecht <eric@ericknecht.com>
2011-09-10 16:11:14 -0400

    fixed video file regex

--- plugin.video.smallscreennetwork/default.py
+++ plugin.video.smallscreennetwork/default.py
@@ -23,14 +23,13 @@ def addDir(name, url, mode, icon, count):
 	ok = xbmcplugin.addDirectoryItem(int(sys.argv[1]), u, liz, isFolder = True, totalItems = count)
 	return ok
 
-def playEpisode(video_url):
-        print video_url
-        liz = xbmcgui.ListItem(name, path = video_url)
-        liz.setInfo(type = "Video", infoLabels = {"Title": name})
+def playEpisode(url):
+        video_data = videolink(url)
+        liz = xbmcgui.ListItem(video_data['title'], path = video_data['video_url'])
+        liz.setInfo(type = "Video", infoLabels = {"Title": video_data['title']})
         liz.setProperty('isPlayable', 'true')
         
         xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, liz)
-	#xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play(str(video_url), liz)
 	
 	
 def shows():
@@ -46,33 +45,59 @@ def shows():
       
                        
 def episodes(url):
+        print "Episodes: " + url
         req = urllib2.Request(url)
         req.add_header('User-Agent',userAgent)
               
         response = urllib2.urlopen(req)
+        
         link=response.read()
+        soup = BeautifulStoneSoup(link)
         response.close()
-        match=re.compile('<a href="(.+?)">\s*<img src="(.+?)".*>\s*<h3>(.+?)</h3>').findall(link)
-        for href, thumb, name in match:
-            addDir(name,site+href,2,site+thumb,1)
+        episodes=re.compile('<a href="(.+?)">\s*<img src="(.+?)".*>\s*<h3>(.+?)</h3>').findall(link)
+       
+        count = len(episodes)
+        
+                
+        prevPage = soup.find('a', text="&lt;")
+       
+        if( prevPage != None):
+                count = count + 1
+               
+                addDir("<< Previous Page", prevPage.parent['href'], 1, '', count)
+		
+		
+        for href, thumb, name in episodes:
+            addLink(name,site+href,site+thumb,count)
+		
+        nextPage = soup.find('a', text="&gt;")
+        
+        if( nextPage != None):
+              
+                addDir("Next Page >>", nextPage.parent['href'], 1, '', count)	
 
-def videolinks(url,name):
+def videolink(url):
         req = urllib2.Request(url)
         req.add_header('User-Agent', userAgent)
         response = urllib2.urlopen(req)
-        link=response.read()
+        page=response.read()
         response.close()
-        match=re.compile("s1\.addVariable\('file','(.+?)'\);").findall(link)
-        video_url = match[0]
-        addLink(name,  video_url, url, '', 1)
+        video_match = re.compile("file: \"(.+?)\",").findall(page)
+        descrip_match = re.compile('<div id="sidebarnarrow">\s*<h2 class="title">(.+?)').findall(page)#</h2>\s*<p>(.+?)<p>
+        recipe_match = re.compile('<div class="half"><h4>(.+?)</h4>').findall(page)
+        result = {}
+        result['video_url'] = video_match[0]
+        result['title'] = descrip_match[0]
+        #result['descrip'] =  descrip_match[1]
+        return result
 
 
-def addLink(name,  url, referer, icon, count):
+def addLink(name,  url, icon, count):
         print url
 	ok = True
-	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&referer=" + urllib.quote_plus(referer) + "&mode=3&name=" + urllib.quote_plus(name)
-	liz = xbmcgui.ListItem(name, iconImage = icon, thumbnailImage = icon)
-	liz.setInfo(type = "Video", infoLabels = {"Title": name})
+	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=3&name=" + urllib.quote_plus(name)
+	liz = xbmcgui.ListItem(cleanHTML(name), iconImage = icon, thumbnailImage = icon)
+	liz.setInfo(type = "Video", infoLabels = {"Title": cleanHTML(name)})
 	liz.setProperty('isPlayable', 'true')
 	ok = xbmcplugin.addDirectoryItem(int(sys.argv[1]), u, liz, isFolder = False, totalItems = count)
 	return ok        
@@ -131,9 +156,7 @@ if mode == None or mode == 0 or url == None or len(url) < 1:
 elif mode==1:
         episodes(url)
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
-elif mode==2:
-        videolinks(url,name)
-	xbmcplugin.endOfDirectory(int(sys.argv[1]), updateListing = True)
+
 elif mode == 3:
 	playEpisode(url)
 
